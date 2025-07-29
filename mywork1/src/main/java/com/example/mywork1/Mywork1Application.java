9cd08ee2ad17c806fbe05934524c275cb295f7c2
sabarim6369 <sabarim6369@gmail.com>
2025-07-29 23:41:54 +0530

    Successfully deployed my first java springboot application

---
+++ mywork1/src/main/java/com/example/mywork1/Mywork1Application.java
@@ -0,0 +1,13 @@
+package com.example.mywork1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mywork1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mywork1Application.class, args);
+	}
+
+}
